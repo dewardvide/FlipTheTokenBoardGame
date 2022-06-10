43bf44c0f90128db23e1da78abd56541cc34edd1
David Omurwa <91884298+dewardvide@users.noreply.github.com>
2022-06-10 10:09:05 +0200

    Update FlipTheTokenBoardGame.py
    instantiation

--- FlipTheTokenBoardGame.py
+++ FlipTheTokenBoardGame.py
@@ -19,14 +19,10 @@ class Admin:
         columns = int(input("Set the desired number of columns: \n"))
         no_tokens = rows * columns
         return no_tokens, rows, columns
-
-def countdown(t = Admin.set_timelimit()):
-   pass
-
 class Board:
-    Total_tokens = Admin.set_number_of_tokens()[0]
-    Time = Admin.set_timelimit()
-    Attempts = Admin.set_max_tries()
+    Total_tokens = Admin.set_number_of_tokens(Admin)[0]
+    Time = Admin.set_timelimit(Admin)
+    Attempts = Admin.set_max_tries(Admin)
 
     def flip(self):
         timeleft = self.Time
@@ -37,18 +33,18 @@ class Board:
                 x = input("Enter the x-axis coordinate of the winning coin: ")
                 y = input("Enter the y-axis coordinate of the winning coin: ")
                 choice = str((x, y))
-                if Tokens.token_coordinates == choice:
+                if choice == Tokens.generate_tokens(Tokens)[2]:
                     Tokens.sides = True
-                if choice == WiningToken.generate_win_token():
+                if choice == WiningToken.generate_win_token(WiningToken):
                     print("You win!")
                     break
             else:
                 print("Invalid choice, token is already flipped!")
 
             current_attempt += 1
-            
+
 class Tokens(Board):
-    def generate_tokens:
+    def generate_tokens(self):
         list_of_tokens = []
         sides = False
         token_coordinates = ""
@@ -56,16 +52,16 @@ class Tokens(Board):
         x = 0
         y = 0
         while generated_tokens < Board.Total_tokens:
-            while x < Admin.set_number_of_tokens()[1]:
+            while x < Admin.set_number_of_tokens(Admin)[1]:
                 x += 1
                 continue
-            while y < Admin.set_number_of_tokens()[2]:
+            while y < Admin.set_number_of_tokens(Admin)[2]:
                 y += 1
                 continue
             token_coordinates = str((x, y))
             list_of_tokens.append(token_coordinates)
             generated_tokens += 1
-        return sides, list_of_tokens
+        return sides, list_of_tokens, token_coordinates
 
 class WiningToken(Tokens):
     def generate_win_token(self):
@@ -74,19 +70,18 @@ class WiningToken(Tokens):
         wining_token = str((row, column))
         return wining_token
 
-
 class Player:
     def play(self):
         while True:
             option = input("Menu\n==========\nSelect and option: \n1.Set up\n2.Play\n3.Quit")
             if option == 1:
-                Admin.set_number_of_tokens()
-                Admin.set_timelimit()
-                Admin.set_max_tries()
-                Admin.set_players()
+                Admin.set_number_of_tokens(Admin)
+                Admin.set_timelimit(Admin)
+                Admin.set_max_tries(Admin)
+                Admin.set_players(Admin)
             elif option == 2:
-                Tokens.generate_tokens()
-                Board.flip()
+                Tokens.generate_tokens(Tokens)
+                Board.flip(Board)
             elif option == 3:
                 quit()
             else:
