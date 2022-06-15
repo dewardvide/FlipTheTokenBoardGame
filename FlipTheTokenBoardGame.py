5a973dd0b5282b3dcc2d367bcd73274040295ec2
Kyle Johnson <johnsonk5@uni.coventry.ac.uk>
2022-06-15 10:48:50 +0200

    Moved input functions to player class, and added instances for admin and board

--- FlipTheTokenBoardGame.py
+++ FlipTheTokenBoardGame.py
@@ -2,38 +2,46 @@ import random
 import time
 
 class Admin:
-    def set_players(self):
-        players = int(input("Enter the number of players: \n"))
-        return players
+    def __init__(self):
+        self.players = 1
+        self.time_limit = 60
+        self.max_tries = 5
+        self.rows = 5
+        self.columns = 5
+        self.no_tokens = 25
 
-    def set_timelimit(self):
-        timelimit = int(input("Set the time limit: \n"))
-        return timelimit
+    def set_players(self, players):
+        self.players = players
+        return self.players
 
-    def set_max_tries(self):
-        max_tries = int(input("Set the maximum amount of tries: \n"))
-        return max_tries
+    def set_time_limit(self, time_limit):
+        self.time_limit = time_limit
+        return self.time_limit
 
-    def set_number_of_tokens(self):
-        rows = int(input("Set the desired number of rows: \n"))
-        columns = int(input("Set the desired number of columns: \n"))
-        no_tokens = rows * columns
-        return no_tokens, rows, columns
+    def set_max_tries(self, max_tries):
+        self.max_tries = max_tries
+        return self.max_tries
+
+    def set_number_of_tokens(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.no_tokens = self.rows * self.columns
+        return self.no_tokens, self.rows, self.columns
+
 class Board:
-    Total_tokens = Admin.set_number_of_tokens(Admin)[0]
-    Time = Admin.set_timelimit(Admin)
-    Attempts = Admin.set_max_tries(Admin)
+    def __init__(self, board_admin):
+        self.total_tokens = board_admin.no_tokens
+        self.time = board_admin.time_limit
+        self.attempts = board_admin.max_tries
 
-    def flip(self):
-        timeleft = self.Time
+    def flip(self, x, y):
+        time_left = self.time
         current_attempt = 0
-        while timeleft == 0 and current_attempt >= self.Attempts:
-            timeleft = timeleft - time.time()
+        while time_left == 0 and current_attempt >= self.attempts:
+            time_left = time_left - time.time()
             if Tokens.sides is False:
-                x = input("Enter the x-axis coordinate of the winning coin: ")
-                y = input("Enter the y-axis coordinate of the winning coin: ")
                 choice = str((x, y))
-                if choice == Tokens.generate_tokens(Tokens)[2]:
+                if choice == Tokens.generate_tokens(Tokens, board_admin)[2]:
                     Tokens.sides = True
                 if choice == WiningToken.generate_win_token(WiningToken):
                     print("You win!")
@@ -44,18 +52,18 @@ class Board:
             current_attempt += 1
 
 class Tokens(Board):
-    def generate_tokens(self):
+    def generate_tokens(self, board_admin):
         list_of_tokens = []
         sides = False
         token_coordinates = ""
         generated_tokens = 0
         x = 0
         y = 0
-        while generated_tokens < Board.Total_tokens:
-            while x < Admin.set_number_of_tokens(Admin)[1]:
+        while generated_tokens < board_admin.no_tokens:
+            while x < board_admin.no_tokens:
                 x += 1
                 continue
-            while y < Admin.set_number_of_tokens(Admin)[2]:
+            while y < board_admin.no_tokens:
                 y += 1
                 continue
             token_coordinates = str((x, y))
@@ -71,18 +79,22 @@ class WiningToken(Tokens):
         return wining_token
 
 class Player:
-    def play(self):
+    def play(self, board_admin, input_board):
         while True:
-            option = input("Menu\n==========\nSelect and option: \n1.Set up\n2.Play\n3.Quit")
+            option = int(input("Menu\n==========\nSelect and option: \n1.Set up\n2.Play\n3.Quit\n"))
             if option == 1:
-                Admin.set_number_of_tokens(Admin)
-                Admin.set_timelimit(Admin)
-                Admin.set_max_tries(Admin)
-                Admin.set_players(Admin)
+                board_admin.set_number_of_tokens(int(input("Set the desired number of rows: \n")), int(input("Set the desired number of columns: \n")))
+                board_admin.set_time_limit(int(input("Set the time limit: \n")))
+                board_admin.set_max_tries(int(input("Set the maximum amount of tries: \n")))
+                board_admin.set_players(int(input("Enter the number of players: \n")))
             elif option == 2:
-                Tokens.generate_tokens(Tokens)
-                Board.flip(Board)
+                Tokens.generate_tokens(Tokens, board_admin)
+                input_board.flip(input("Enter the x-axis coordinate of the winning coin: "), input("Enter the y-axis coordinate of the winning coin: "))
             elif option == 3:
                 quit()
             else:
                 print("Invalid values!")
+
+admin1 = Admin()
+board1 = Board(admin1)
+Player.play(Player, admin1, board1)
